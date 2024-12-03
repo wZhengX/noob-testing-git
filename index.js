d07abf85873cfd29fae9b81d0950f883d78191ff
Zheng Xian <wongzhengxian64@gmail.com>
2024-12-03 08:28:20 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+//my first js code
+
+
+let count=0;
+
+let countEL=document.getElementById("count-el");
+
+function increment(){
+    count=count+1;
+    countEL.innerText=count;
+}
+
+function save(){
+
+    let countStr=count + "-";
+    let message=document.getElementById("message-el")
+    message.textContent+= countStr;
+    count=0;
+
+}
+
